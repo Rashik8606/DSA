626f54711b8488abda8f3e25524f8cae6cb7884f
rashik <mhdrashik112@gmail.com>
2025-09-26 21:14:32 +0530

    test

--- Arrays/largestNumber.py
+++ Arrays/largestNumber.py
@@ -1,2 +1,10 @@
-def largest_Number():
-    pass
+def largest_number_smallest_number(arr):
+    result = arr.sort()
+
+    smallest = arr[0]
+    largest = arr[-1]
+
+    return smallest, largest
+
+lis = [1,28,34,67,89,43,90]
+print(largest_number_smallest_number(lis))
